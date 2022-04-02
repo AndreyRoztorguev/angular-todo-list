dc7f6c2ae052f59f6ae963682d965e73fd579aa5
Andrey Roztorguev <andrew.roztor99@gmail.com>
2022-04-02 15:12:50 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Todo } from './shared/todos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-youtube';
+  appTitle = 'Angular todo application';
 }
